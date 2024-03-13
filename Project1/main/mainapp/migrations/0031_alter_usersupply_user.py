03f85179a85d45eea5ed3496b1dc68adaedbfe44
laineyyrose <lmrose5522@gmail.com>
2024-03-13 00:19:58 -0400

    trying to deploy

--- Project1/main/mainapp/migrations/0031_alter_usersupply_user.py
+++ Project1/main/mainapp/migrations/0031_alter_usersupply_user.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="usersupply",
             name="user",
-            field=models.OneToOneField(
+            field=models.ForeignKey(
                 on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL
             ),
         ),
